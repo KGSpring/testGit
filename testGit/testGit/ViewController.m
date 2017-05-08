6a4d9980dab08581615927ef64dd48c5bad6f2b2
KGSpring <2464750251@qq.com>
2017-05-08 10:12:25 +0800

    m

--- testGit/testGit/ViewController.m
+++ testGit/testGit/ViewController.m
@@ -25,11 +25,8 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
     NSLog(@"第一次");
-<<<<<<< HEAD
     NSLog(@"第二次");
     NSLog(@"第三次");
-=======
->>>>>>> parent of a37c793... 第二次
 }
 
 
